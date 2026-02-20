dad51611c89fd1629ea4e754db7a0f12431c0495
Pfeister47 <john.pfeister@smartshoot.com>
2026-02-19 23:55:37 -0500

    fix build - isolate api from frontend bundle

--- vite.config.js
+++ vite.config.js
@@ -3,12 +3,4 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  build: {
-    rollupOptions: {
-      external: ['googleapis', 'google-auth-library', 'node-fetch', 'stream', 'util', 'buffer'],
-    },
-  },
-  optimizeDeps: {
-    exclude: ['googleapis', 'google-auth-library'],
-  },
 })
