67df65a6973935e80623b807e8c91ebfa3460a29
Pfeister47 <john.pfeister@smartshoot.com>
2026-02-20 00:08:14 -0500

    fix App.jsx and vite config

--- vite.config.js
+++ vite.config.js
@@ -1,17 +1,6 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import { fileURLToPath } from 'url'
-import { dirname, resolve } from 'path'
-
-const __dirname = dirname(fileURLToPath(import.meta.url))
 
 export default defineConfig({
   plugins: [react()],
-  resolve: {
-    alias: {
-      googleapis: resolve(__dirname, 'src-stub-server.js'),
-      'google-auth-library': resolve(__dirname, 'src-stub-server.js'),
-      'node-fetch': resolve(__dirname, 'src-stub-server.js'),
-    },
-  },
 })
